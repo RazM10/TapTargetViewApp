580676dc87da9eeb954d43ecc50f91325ff588a1
Razaul Munir <razaulmunir@gmail.com>
2019-12-24 15:55:43 +0600

    Tap target view for one item is done and another activity added for TapTargetSequence

--- app/src/main/java/org/myself/taptargetviewpractice/MainActivity.java
+++ app/src/main/java/org/myself/taptargetviewpractice/MainActivity.java
@@ -2,22 +2,46 @@ package org.myself.taptargetviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_helloWorld;
     Button btn_show;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_helloWorld=findViewById(R.id.tv_helloWorld);
-        btn_show=findViewById(R.id.btn_show);
+        tv_helloWorld = findViewById(R.id.tv_helloWorld);
+        btn_show = findViewById(R.id.btn_show);
 
+        TapTargetView.showFor(this,
+                TapTarget.forView(tv_helloWorld, "Title", "It is a description")
+                        .outerCircleColor(R.color.colorAccent)
+                        .cancelable(false),
+                new TapTargetView.Listener() {
+                    @Override
+                    public void onTargetClick(TapTargetView view) {
+                        super.onTargetClick(view);
+                        view.dismiss(true);
+                    }
+                }
+        );
 
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(),));
+            }
+        });
     }
 }
