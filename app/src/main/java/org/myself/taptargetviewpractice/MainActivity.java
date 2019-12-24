c0dc6acd4c19840fcf201582d18e576fcf14474d
Razaul Munir <razaulmunir@gmail.com>
2019-12-24 15:33:22 +0600

    design xml file of main activity

--- app/src/main/java/org/myself/taptargetviewpractice/MainActivity.java
+++ app/src/main/java/org/myself/taptargetviewpractice/MainActivity.java
@@ -3,12 +3,21 @@ package org.myself.taptargetviewpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_helloWorld;
+    Button btn_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_helloWorld=findViewById(R.id.tv_helloWorld);
+        btn_show=findViewById(R.id.btn_show);
+
+
     }
 }
