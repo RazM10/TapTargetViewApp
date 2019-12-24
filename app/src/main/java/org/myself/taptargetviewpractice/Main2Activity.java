72b4a9acbe11e99fbd873cc0a48f6d7c58e8a6ea
Razaul Munir <razaulmunir@gmail.com>
2019-12-24 16:21:52 +0600

    Xml design of Activity2 done

--- app/src/main/java/org/myself/taptargetviewpractice/Main2Activity.java
+++ app/src/main/java/org/myself/taptargetviewpractice/Main2Activity.java
@@ -3,12 +3,19 @@ package org.myself.taptargetviewpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+
     }
 }
